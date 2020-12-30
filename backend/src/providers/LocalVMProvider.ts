403ec2cc2d50324ce65cac5994dfd255ea767c85
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2020-12-30 12:48:27 +0100

    added support to change ssh port of vm/p4 host, addressing #20

--- backend/src/providers/LocalVMProvider.ts
+++ backend/src/providers/LocalVMProvider.ts
@@ -1,17 +1,23 @@
 import { InstanceProvider, VMEndpoint } from "./Provider";
 export default class OpenStackProvider implements InstanceProvider {
-  private ipAddress: string;
+  private ipAddress: string = "127.0.0.1";
+  private sshPort: number = 22;
   constructor() {
-    this.ipAddress = process.env.VBOX_IP_ADDRESS;
+    if (process.env.VBOX_IP_ADDRESS != undefined) {
+      this.ipAddress = process.env.VBOX_IP_ADDRESS;
+    }
+    if (process.env.VBOX_SSH_PORT != undefined) {
+      this.sshPort = parseInt(process.env.VBOX_SSH_PORT);
+    }
     console.log(
-      `VirtualBoxProvider VBOX_IP_ADDRESS from env: ${this.ipAddress}`
+      `Using LocalVMProvider with ipAddress: ${this.ipAddress} and sshPort: ${this.sshPort}`
     );
   }
 
   async getServer(): Promise<VMEndpoint> {
     return {
       IPAddress: this.ipAddress,
-      SSHPort: 22,
+      SSHPort: this.sshPort,
       LanguageServerPort: 3005,
       identifier: "vbox",
     };
