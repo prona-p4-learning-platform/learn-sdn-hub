6b28fb7f12a2e5dddaabfab7826317b7b8e094a6
anxietypb <anxiety.pb@gmail.com>
2021-01-01 13:26:25 +0100

    improvement: backend can serve static files for /

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -3,6 +3,8 @@ import { createServer } from "http";
 import express, { Router } from "express";
 import cors from "cors";
 import { errors } from "celebrate";
+import path from "path";
+import history from 'connect-history-api-fallback'
 export default function (api: Router): void {
   const port = 3001;
   const app = express();
@@ -13,6 +15,10 @@ export default function (api: Router): void {
   app.use(cors());
   app.use(api);
   app.use(errors());
+  const frontendStaticDir = path.resolve(__dirname, "..", "static");
+  console.log("Serving static html from "+frontendStaticDir)
+  app.use(history())
+  app.use(express.static(frontendStaticDir));
   server.listen(port, function () {
     console.log(`HTTP Server listening on port ${port}`);
   });
