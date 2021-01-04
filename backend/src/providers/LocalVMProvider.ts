c4512db525e4a2a2ccc97a053fe52786334d0969
anxietypb <anxiety.pb@gmail.com>
2021-01-04 21:51:04 +0100

    improvement: allow multiple VMs to be provided by LocalVMProvider

--- backend/src/providers/LocalVMProvider.ts
+++ backend/src/providers/LocalVMProvider.ts
@@ -1,23 +1,37 @@
 import { InstanceProvider, VMEndpoint } from "./Provider";
 export default class OpenStackProvider implements InstanceProvider {
   private ipAddress: string;
+  private availableInstances: Map<string, VMEndpoint> = new Map();
+  private availableInstancesList: string[] = [];
   constructor() {
     this.ipAddress = process.env.VBOX_IP_ADDRESS;
     console.log(
       `VirtualBoxProvider VBOX_IP_ADDRESS from env: ${this.ipAddress}`
     );
-  }
-
-  async getServer(): Promise<VMEndpoint> {
-    return {
+    this.availableInstances.set("vbox", {
       IPAddress: this.ipAddress,
       SSHPort: 22,
       LanguageServerPort: 3005,
       identifier: "vbox",
-    };
+    });
+    this.availableInstances.set("vbox2", {
+      IPAddress: "192.168.178.108",
+      SSHPort: 22,
+      LanguageServerPort: 3005,
+      identifier: "vbox2",
+    });
+    this.availableInstancesList = ["vbox", "vbox2"];
+  }
+
+  async getServer(identifier: string): Promise<VMEndpoint> {
+    return this.availableInstances.get(identifier);
   }
 
   async createServer(): Promise<VMEndpoint> {
-    return this.getServer();
+    if (this.availableInstancesList.length > 0) {
+      console.log(this.availableInstancesList);
+      return this.availableInstances.get(this.availableInstancesList.pop());
+    }
+    throw new Error("Cannot create server.");
   }
 }
