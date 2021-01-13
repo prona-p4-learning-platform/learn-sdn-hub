6fcb1ab72fe32f090e18253cf4fbaa9542c702c1
flex <sebastian.rieger@informatik.hs-fulda.de>
2021-01-13 15:05:11 +0100

    Support multiple users and mapping for localvm (#32)
    * initial implementation
    
    * select instance based on mappingadded check and user feedback for failed deployment
    
    * respect lint errors
    
    * respect further lint errors
    
    * respect further lint errors
    
    * respect further lint errors
    
    * respect further lint errors

--- backend/src/authentication/PlaintextAuthenticationProvider.ts
+++ backend/src/authentication/PlaintextAuthenticationProvider.ts
@@ -9,8 +9,24 @@ export default class PlaintextAuthenticationProvider
     username: string,
     password: string
   ): Promise<AuthenticationResult> {
-    if (password === "p4") {
-      return { username: username, userid: username, type: "plain" };
+    if (process.env.BACKEND_USERS != undefined) {
+      const usersConfig = process.env.BACKEND_USERS.split(",");
+      const users: Map<string, string> = new Map();
+      usersConfig.forEach((user) => {
+        const login = user.split(":")[0];
+        const password = user.split(":")[1];
+        users.set(login, password);
+      });
+
+      if (users.has(username)) {
+        if (password === users.get(username)) {
+          return { username: username, userid: username, type: "plain" };
+        }
+      }
+    } else {
+      if (password === "p4") {
+        return { username: username, userid: username, type: "plain" };
+      }
     }
     throw new Error("AuthenticationError");
   }
--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -65,7 +65,7 @@ export default function(props: LoginFormProps) {
             onSuccessfulAuthentication(a.token, a.username)
             history.push("/assignments")
         } else if (result.status===401){
-            setLoginResult("Auth failed")
+            setLoginResult("Auth failed!")
             setLoginSeverity("error")
             setNotificationOpen(true)
         }
