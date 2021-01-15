09ac4204c1454da94b58ccd0fef569a5f0ad3b56
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2021-01-15 13:14:54 +0100

    introduced new LocalMultiuserVM approach, as discussed in #30

--- backend/src/authentication/PlaintextAuthenticationProvider.ts
+++ backend/src/authentication/PlaintextAuthenticationProvider.ts
@@ -9,24 +9,8 @@ export default class PlaintextAuthenticationProvider
     username: string,
     password: string
   ): Promise<AuthenticationResult> {
-    if (process.env.BACKEND_USERS != undefined) {
-      const usersConfig = process.env.BACKEND_USERS.split(",");
-      const users: Map<string, string> = new Map();
-      usersConfig.forEach((user) => {
-        const login = user.split(":")[0];
-        const password = user.split(":")[1];
-        users.set(login, password);
-      });
-
-      if (users.has(username)) {
-        if (password === users.get(username)) {
-          return { username: username, userid: username, type: "plain" };
-        }
-      }
-    } else {
-      if (password === "p4") {
-        return { username: username, userid: username, type: "plain" };
-      }
+    if (password === "p4") {
+      return { username: username, userid: username, type: "plain" };
     }
     throw new Error("AuthenticationError");
   }
