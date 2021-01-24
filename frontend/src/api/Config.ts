4c3760e0e69316687bf5c8b658c1b1ceb019ba64
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2021-01-24 01:04:12 +0100

    fixed dynamic generation of backendURL. Backend connection only worked when using REACT_APP_API_URL and REACT_APP_WS_URL etc. in .env.local. Config.ts should generate backend connection, and this will not be 3000 but rather 3001 by default. Also added option to adapt changed backend port. Also window.location.protocol already contains trailing :, so the code led to http://localhost:3000/http:://localhost:3001/some/api/dir being created.

--- frontend/src/api/Config.ts
+++ frontend/src/api/Config.ts
@@ -1,9 +1,9 @@
 const hostname = window?.location?.hostname ?? "localhost";
-const port = window?.location?.port ?? "3001";
+const port = process.env.REACT_APP_BACKEND_HTTP_PORT ?? "3001";
 const protocol = window?.location?.protocol ?? "http";
-const backendURL = process.env.REACT_APP_API_HOST ?? `${protocol}://${hostname}:${port}`;
-const wsProtocol = protocol === "http" ? "ws": "wss"
-const wsBackendHost = process.env.REACT_APP_WS_HOST ?? `${wsProtocol}://${hostname}:${port}`;
+const backendURL = process.env.REACT_APP_API_HOST ?? `${protocol}//${hostname}:${port}`;
+const wsProtocol = protocol === "http:" ? "ws:": "wss:"
+const wsBackendHost = process.env.REACT_APP_WS_HOST ?? `${wsProtocol}//${hostname}:${port}`;
 
 export default {
     backendURL,
