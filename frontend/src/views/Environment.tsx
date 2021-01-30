40284c47282c660f0c09f650fbe5360f249e06ba
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2021-01-30 16:48:45 +0100

    removed unused import

--- frontend/src/views/Environment.tsx
+++ frontend/src/views/Environment.tsx
@@ -18,7 +18,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import { keys } from "@material-ui/core/styles/createBreakpoints";
 
 type Severity = "error" | "success" | "info" | "warning" | undefined;
 
