34908939c74b7e4e9d09bfa389630ab0d63bbff4
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2021-03-19 18:24:36 +0100

    respect ci/cd hint

--- frontend/src/components/EditorTabs.tsx
+++ frontend/src/components/EditorTabs.tsx
@@ -3,7 +3,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import P4Editor from "./P4Editor";
 
 interface EditorTabsProps {
   children?: React.ReactNode;
