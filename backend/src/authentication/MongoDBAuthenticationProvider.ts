41c16e1ba415dbbb17901bb8624b6d4e5913ea27
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2023-07-02 22:48:20 +0200

    added support to configure assignment filter in the db instead of only env var, db filter precedes env var, fixed exception in backend if submissions for user or group cannot be retrieved

--- backend/src/authentication/MongoDBAuthenticationProvider.ts
+++ backend/src/authentication/MongoDBAuthenticationProvider.ts
@@ -70,28 +70,39 @@ export default class MongoDBAuthenticationProvider
     username: string,
     assignmentList: Map<string, EnvironmentDescription>
   ): Promise<Map<string, EnvironmentDescription>> {
-    const usersAllowedAssignments =
-      process.env.BACKEND_USER_ALLOWED_ASSIGNMENTS;
-    if (usersAllowedAssignments == undefined) {
+    const user = await this.persister.GetUserAccount(username);
+    if (user.assignmentListFilter != undefined) {
+      const tempRegex = user.assignmentListFilter;
+      for (const key of assignmentList.keys()) {
+        if (key.match(tempRegex) == null) {
+          assignmentList.delete(key);
+        }
+      }
       return assignmentList;
     } else {
-      const users: Map<string, string> = new Map();
-      usersAllowedAssignments.split(",").forEach((user) => {
-        const name = user.split(":")[0];
-        const regex = user.split(":")[1];
-        users.set(name, regex);
-      });
-      if (users.has(username)) {
-        const tempRegex = users.get(username);
-        for (const key of assignmentList.keys()) {
-          if (key.match(tempRegex) == null) {
-            assignmentList.delete(key);
-          }
-        }
+      const usersAllowedAssignments =
+        process.env.BACKEND_USER_ALLOWED_ASSIGNMENTS;
+      if (usersAllowedAssignments == undefined) {
         return assignmentList;
       } else {
-        assignmentList.clear();
-        return assignmentList;
+        const users: Map<string, string> = new Map();
+        usersAllowedAssignments.split(",").forEach((user) => {
+          const name = user.split(":")[0];
+          const regex = user.split(":")[1];
+          users.set(name, regex);
+        });
+        if (users.has(username)) {
+          const tempRegex = users.get(username);
+          for (const key of assignmentList.keys()) {
+            if (key.match(tempRegex) == null) {
+              assignmentList.delete(key);
+            }
+          }
+          return assignmentList;
+        } else {
+          assignmentList.clear();
+          return assignmentList;
+        }
       }
     }
   }
