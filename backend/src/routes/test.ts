759179d9e83c84702f5747e03594cdf7665f3da4
David Hermann <davidhermann2003@icloud.com>
2024-01-23 19:30:14 +0100

    Fixed prettier issues

--- backend/src/routes/test.ts
+++ backend/src/routes/test.ts
@@ -2,39 +2,38 @@ import { exec } from "child_process";
 import { Router } from "express";
 
 export default (): Router => {
-	const router = Router();
+  const router = Router();
 
-	router.get("/", (_, res) => {
-		const ymlPath =
-			"<path-to-topo.yml>"; // Test path to topology
+  router.get("/", (_, res) => {
+    const ymlPath = "<path-to-topo.yml>"; // Test path to topology
 
-		function execAsync(command: string) {
-			return new Promise((resolve, reject) => {
-				exec(command, (error: any, stdout: any, stderr: any) => {
-					if (error) {
-						reject(`Ausführungsfehler: ${error}`);
-						return;
-					}
-					resolve({ stdout, stderr });
-				});
-			});
-		}
+    function execAsync(command: string) {
+      return new Promise((resolve, reject) => {
+        exec(command, (error: any, stdout: any, stderr: any) => {
+          if (error) {
+            reject(`Ausführungsfehler: ${error}`);
+            return;
+          }
+          resolve({ stdout, stderr });
+        });
+      });
+    }
 
-		async function runCommandsSequentially() {
-            console.log("Hello world this is containerlab endpoint") // Does not get printed
-			try {
-				const result2: any = await execAsync(
-					`sudo containerlab graph --topo ${ymlPath} --srv ":3003"`
-				);
-				console.log("Befehl ausgeführt:", result2.stdout); // Does not get printed
-			} catch (error) {
-				console.error("Fehler:", error);
-			}
-		}
+    async function runCommandsSequentially() {
+      console.log("Hello world this is containerlab endpoint"); // Does not get printed
+      try {
+        const result2: any = await execAsync(
+          `sudo containerlab graph --topo ${ymlPath} --srv ":3003"`
+        );
+        console.log("Befehl ausgeführt:", result2.stdout); // Does not get printed
+      } catch (error) {
+        console.error("Fehler:", error);
+      }
+    }
 
-		runCommandsSequentially();
-        res.status(200); // Status 200 gets returned
-	});
+    runCommandsSequentially();
+    res.status(200); // Status 200 gets returned
+  });
 
-	return router;
+  return router;
 };
