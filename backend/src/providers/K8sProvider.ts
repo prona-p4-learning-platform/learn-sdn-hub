c05b274fb10f0220bf39b0389b6250fbc5ad2c0e
Ylie2003 <yannisliebermann@gmail.com>
2025-11-21 14:50:33 +0100

    fix eslint

--- backend/src/providers/K8sProvider.ts
+++ backend/src/providers/K8sProvider.ts
@@ -1,13 +1,13 @@
 import { InstanceProvider, VMEndpoint } from "./Provider";
 
 export default class K8sProvider implements InstanceProvider {
-  createServer(username: string, groupNumber: number, environment: string, options?: { image?: string; dockerCmd?: string; dockerSupplementalPorts?: string[]; kernelImage?: string; kernelBootARGs?: string; rootDrive?: string; proxmoxTemplateTag?: string; mountKubeconfig?: boolean; sshTunnelingPorts?: string[]; }): Promise<VMEndpoint> {
+  createServer(_username: string, _groupNumber: number, _environment: string, _options?: { image?: string; dockerCmd?: string; dockerSupplementalPorts?: string[]; kernelImage?: string; kernelBootARGs?: string; rootDrive?: string; proxmoxTemplateTag?: string; mountKubeconfig?: boolean; sshTunnelingPorts?: string[]; }): Promise<VMEndpoint> {
     throw new Error("Method not implemented.");
   }
-  getServer(instance: string): Promise<VMEndpoint> {
+  getServer(_instance: string): Promise<VMEndpoint> {
     throw new Error("Method not implemented.");
   }
-  deleteServer(instance: string): Promise<void> {
+  deleteServer(_instance: string): Promise<void> {
     throw new Error("Method not implemented.");
   }
   
