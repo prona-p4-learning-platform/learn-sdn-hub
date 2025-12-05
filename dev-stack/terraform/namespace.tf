2c8f542f3794042197c4e5597d61e0ae74562f2c
cxnnqr <connor.neubauer@icloud.com>
2025-12-05 15:01:40 +0100

    refactoring

--- dev-stack/terraform/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace_v1" "learn-sdn-hub" {
-  metadata {
-    name = "learn-sdn-hub"
-  }
-}
--- dev-stack/terraform/nginx-ingress-controller.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "nginx-ingress-controller" {
-  name = "nginx-ingress-controller"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart = "ingress-nginx"
-  version = "4.14.0"
-  namespace = "nginx-controller"
-  create_namespace = true
-}
