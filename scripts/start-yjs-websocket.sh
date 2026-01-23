c93424202fac924b6b66c4915ee73db1c8ec9e5a
fdai7848 <jan.heidekrueger@informatik.hs-fulda.de>
2026-01-23 14:52:47 +0100

    Deleted scripts folder

--- scripts/start-yjs-websocket.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /root/.nvm/nvm.sh
-HOST=10.33.19.13 PORT=11005 npx y-websocket
-# can be used as: ws://localhost:1234
-
-#with persistence (keeping document content for group after restart of environement and/or server):
-#HOST=localhost PORT=1234 YPERSISTENCE=./dbDir node ./node_modules/y-websocket/bin/server.js
